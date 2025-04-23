61228a5f040b1a5b4f456bdd951b85bb416c2ee9
Andreas Mouzong <andreasmouzong@gmail.com>
2025-04-23 22:20:59 +0200

    Initialisation du projet

---
+++ corpo-chat/src/main/java/com/adacorp/corpochat/CorpoChatApplication.java
@@ -0,0 +1,13 @@
+package com.adacorp.corpochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpoChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpoChatApplication.class, args);
+	}
+
+}
---
+++ corpo-chat/src/test/java/com/adacorp/corpochat/CorpoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adacorp.corpochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpoChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
