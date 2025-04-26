a1851f5b5ec000150c4f4e9862e9506d060f7dad
Andreas MOUZONG <andreasmouzong@gmail.com>
2025-04-26 11:10:04 +0200

    Added NamedQueries for Message entity

---
+++ corpo-chat/src/main/java/com/adacorp/corpochat/message/MessageConstants.java
@@ -0,0 +1,9 @@
+package com.adacorp.corpochat.message;
+
+public class MessageConstants {
+
+    public static final String FIND_MESSAGES_BY_CHAT_ID = "Message.findMessageByMessageId";
+    public static final String SET_MESSAGES_TO_SEEN_BY_CHAT = "Message.setMessagesToSeenByChat";
+
+    private MessageConstants() {}
+}
